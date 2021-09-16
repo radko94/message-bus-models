18d1bb4a1b062977d8fbdf24710ac0c232dcf059
Radko94 <rapostolov94@gmail.com>
2021-09-16 20:35:14 +0300

    bruuh, imports again

--- helpers/BufferHandler.ts
+++ helpers/BufferHandler.ts
@@ -1,7 +1,7 @@
-import { IBufferInfo } from "interfaces/buffer-info.interface";
-import { IConsumedDataStep } from "interfaces/consumed-data.interface";
-import { IMaskStep } from "interfaces/mask-step.interface";
-import { IPayloadStep } from "interfaces/payload-step.interface";
+import { IBufferInfo } from "../interfaces/buffer-info.interface";
+import { IConsumedDataStep } from "../interfaces/consumed-data.interface";
+import { IMaskStep } from "../interfaces/mask-step.interface";
+import { IPayloadStep } from "../interfaces/payload-step.interface";
 
 export class BufferHandler {
   private _bufferedBytes: number;
